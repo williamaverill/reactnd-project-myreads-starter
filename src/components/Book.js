e24d5961236b4dbf7be48cfd8f84552ed748db0c
Will Averill <wsaverill@gmail.com>
2022-01-13 06:26:15 -0800

    Add functionality to move books.

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Book extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { shelfValue: this.props.book.shelf };
+    this.shelfChange = this.shelfChange.bind(this);
+  }
+  shelfChange(event) {
+    this.setState({ shelfValue: event.target.value }, () =>
+      this.props.moveBook(this.props.book.id, this.state.shelfValue)
+    );
+  }
   render() {
     return (
       <div className="book">
@@ -10,11 +21,11 @@ class Book extends Component {
             style={{
               width: 128,
               height: 188,
-              backgroundImage: this.props.backgroundImage,
+              backgroundImage: this.props.book.backgroundImage,
             }}
           />
           <div className="book-shelf-changer">
-            <select>
+            <select value={this.state.shelfValue} onChange={this.shelfChange}>
               <option value="move" disabled>
                 Move to...
               </option>
@@ -25,11 +36,16 @@ class Book extends Component {
             </select>
           </div>
         </div>
-        <div className="book-title">{this.props.title}</div>
-        <div className="book-authors">{this.props.author}</div>
+        <div className="book-title">{this.props.book.title}</div>
+        <div className="book-authors">{this.props.book.author}</div>
       </div>
     );
   }
 }
+
+Book.propTypes = {
+  book: PropTypes.object.isRequired,
+  moveBook: PropTypes.func.isRequired,
+};
 
 export default Book;
---
+++ src/components/Bookshelf.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import Book from "./Book";
+
+class Bookshelf extends Component {
+  constructor(props) {
+    super(props);
+    this.moveBook = this.moveBook.bind(this);
+  }
+  moveBook(id, shelf) {
+    this.props.moveBook(id, shelf);
+  }
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.map((book) => {
+              return (
+                <li>
+                  <Book book={book} moveBook={this.moveBook} />
+                </li>
+              );
+            })}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+Bookshelf.propTypes = {
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
+  moveBook: PropTypes.func.isRequired,
+};
+
+export default Bookshelf;
